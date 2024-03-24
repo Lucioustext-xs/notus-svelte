d0a028060a97514c2c7b6bd3a8479cab2c5117f0
Lucioustext-xs <164760429+Lucioustext-xs@users.noreply.github.com>
2024-03-24 14:09:29 +0000

    Update Login.svelte

--- src/views/auth/Login.svelte
+++ src/views/auth/Login.svelte
@@ -9,12 +9,15 @@
 
 <div class="container mx-auto px-4 h-full">
   <div class="flex content-center items-center justify-center h-full">
-    <div class="w-full lg:w-4/12 px-4">
+    <div class="w-full h-full lg:w-4/12 px-4">
       <div
         class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
       >
         <div class="rounded-t mb-0 px-6 py-6">
           <div class="text-center mb-3">
+            <h6 class="text-blueGray-500 text-sm font-bold">
+              Authentication Flow
+            </h6>
             <h6 class="text-blueGray-500 text-sm font-bold">
               Sign in with
             </h6>
